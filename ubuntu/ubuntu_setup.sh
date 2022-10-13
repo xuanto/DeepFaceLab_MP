46ba2496d282512ecd2a6d6dd7e994ebe658d527
xuanto <notagod@foxmail.com>
2022-10-13 21:28:28 +0800

    update code

--- ubuntu/ubuntu_setup.sh
+++ ubuntu/ubuntu_setup.sh
@@ -7,6 +7,19 @@ set -e
 mkdir -p .dfl
 mkdir -p workspace
 
+# init conda
+__conda_setup="$('/home/ubuntu/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/ubuntu/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/ubuntu/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/ubuntu/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
 conda info
 
 echo "Running on $(dirname $0)"
