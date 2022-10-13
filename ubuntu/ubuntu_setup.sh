ab7e517227d7ecb79f577003988cbcb5b3eadd50
xuanto <notagod@foxmail.com>
2022-10-13 23:00:20 +0800

    update code

--- ubuntu/ubuntu_setup.sh
+++ ubuntu/ubuntu_setup.sh
@@ -8,22 +8,22 @@ mkdir -p .dfl
 mkdir -p workspace
 
 # init conda
-echo "$('/home/ubuntu/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-__conda_setup="$('/home/ubuntu/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-echo "debug 1"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/ubuntu/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/ubuntu/anaconda3/etc/profile.d/conda.sh"
-    else
-        echo "debug 2"
-        export PATH="/home/ubuntu/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
+# echo "$('/home/ubuntu/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+# __conda_setup="$('/home/ubuntu/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+# echo "debug 1"
+# if [ $? -eq 0 ]; then
+#     eval "$__conda_setup"
+# else
+#     if [ -f "/home/ubuntu/anaconda3/etc/profile.d/conda.sh" ]; then
+#         . "/home/ubuntu/anaconda3/etc/profile.d/conda.sh"
+#     else
+#         echo "debug 2"
+#         export PATH="/home/ubuntu/anaconda3/bin:$PATH"
+#     fi
+# fi
+# unset __conda_setup
 
-conda info
+$CONDA_EXE info
 
 echo "Running on $(dirname $0)"
 
@@ -37,18 +37,18 @@ fi
 
 if [ ! -d /home/ubuntu/anaconda3/envs/dfl ]; then
   echo "creating dfl env (using python verison=3.9) ..."
-  conda create -n dfl python=3.9
+  $CONDA_EXE create -n dfl python=3.9
 else
   echo "dfl env exists"
 fi
 
-conda activate dfl
+$CONDA_EXE activate dfl
 
 if [ -n "$DFL_MAIN" ]; then
-  conda env config vars set DFL_MAIN=".dfl/DeepFaceLab/main.py"
+  $CONDA_EXE env config vars set DFL_MAIN=".dfl/DeepFaceLab/main.py"
 fi
 if [ -n "$WORKSPACE" ]; then
-  conda env config vars set WORKSPACE="workspace"
+  $CONDA_EXE env config vars set WORKSPACE="workspace"
 fi
 
 echo "upgrading pip ..."
@@ -60,7 +60,7 @@ echo "Using $reqs_file for $(python -V)"
 
 pip --no-cache-dir install -r $reqs_file
 
-conda deactivate
+$CONDA_EXE deactivate
 
 if [ ! -d workspace ]; then
   mkdir -p workspace/data_src
