e3c3d2b18c0d50e982f13978a0e897d3d18f15ff
xuanto <notagod@foxmail.com>
2022-10-13 21:12:34 +0800

    update code

--- ubuntu/ubuntu_setup.sh
+++ ubuntu/ubuntu_setup.sh
@@ -7,6 +7,38 @@ set -e
 mkdir -p .dfl
 mkdir -p workspace
 
+conda () {
+  if [ "$#" -lt 1 ]
+  then
+    "$CONDA_EXE" $_CE_M $_CE_CONDA
+  else
+    \local cmd="$1"
+    shift
+    case "$cmd" in
+      (activate | deactivate) __conda_activate "$cmd" "$@" ;;
+      (install | update | upgrade | remove | uninstall) CONDA_INTERNAL_OLDPATH="${PATH}"
+        __add_sys_prefix_to_path
+        "$CONDA_EXE" $_CE_M $_CE_CONDA "$cmd" "$@"
+        \local t1=$?
+        PATH="${CONDA_INTERNAL_OLDPATH}"
+        if [ $t1 = 0 ]
+        then
+          __conda_reactivate
+        else
+          return $t1
+        fi ;;
+      (*) CONDA_INTERNAL_OLDPATH="${PATH}"
+        __add_sys_prefix_to_path
+        "$CONDA_EXE" $_CE_M $_CE_CONDA "$cmd" "$@"
+        \local t1=$?
+        PATH="${CONDA_INTERNAL_OLDPATH}"
+        return $t1 ;;
+    esac
+  fi
+}
+
+conda info
+
 echo "Running on $(dirname $0)"
 
 if [ ! -d .dfl/DeepFaceLab ]; then
