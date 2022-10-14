2689bf75e9ffdcde6a7244be5ad3b27a479d5ed8
xuanto <notagod@foxmail.com>
2022-10-14 18:39:16 +0800

    update code

--- ubuntu/ubuntu_setup.sh
+++ ubuntu/ubuntu_setup.sh
@@ -25,7 +25,7 @@ else
 fi
 
 echo "CONDA_DEFAULT_ENV=$CONDA_DEFAULT_ENV"
-tmpv = `conda info --envs | grep dfl`
+tmpv=`conda info --envs | grep dfl`
 echo "tmpv=$tmpv"
 if [ ! -n "$tmpv" ]; then
   echo "creating dfl env (using python verison=3.9) ..."
