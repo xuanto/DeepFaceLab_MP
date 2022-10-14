f1b5feb85630baac8cc0d7b4df2972f4ab0e5337
xuanto <notagod@foxmail.com>
2022-10-14 15:57:37 +0800

    update code

--- ubuntu/ubuntu_setup.sh
+++ ubuntu/ubuntu_setup.sh
@@ -12,7 +12,7 @@ if [ ! -n "$CONDA_EXE" ]; then
   exit 1
 fi
 
-$CONDA_EXE info
+conda info
 
 echo "Running on $(dirname $0)"
 
@@ -24,37 +24,12 @@ else
   echo "DeepFaceLab existed, skip cloning ..."
 fi
 
-if [ ! -d /home/ubuntu/anaconda3/envs/dfl ]; then
+tmpv=`conda info --envs | grep dfl`
+if [ ! -n $tmpv ]; then
   echo "creating dfl env (using python verison=3.9) ..."
-  $CONDA_EXE create -n dfl python=3.9
+  conda create -n dfl python=3.9
 else
   echo "dfl env exists"
-fi
-
-$CONDA_EXE activate dfl
-
-if [ -n "$DFL_MAIN" ]; then
-  $CONDA_EXE env config vars set DFL_MAIN=".dfl/DeepFaceLab/main.py"
-fi
-if [ -n "$WORKSPACE" ]; then
-  $CONDA_EXE env config vars set WORKSPACE="workspace"
-fi
-
-echo "upgrading pip ..."
-python -m pip install --upgrade pip
-
-reqs_file='$(dirname $0)/requirements.txt'
-
-echo "Using $reqs_file for $(python -V)"
-
-pip --no-cache-dir install -r $reqs_file
-
-$CONDA_EXE deactivate
-
-if [ ! -d workspace ]; then
-  mkdir -p workspace/data_src
-  mkdir -p workspace/data_dst
-  mkdir -p workspace/model
 fi
 
 echo "successfully install deepfacelab!"
