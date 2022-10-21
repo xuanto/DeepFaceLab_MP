9ade10db2e32b1322a3687f7dc2bdf490047aa96
xuanto <notagod@foxmail.com>
2022-10-21 17:49:44 +0800

    update code

---
+++ linux/setup2.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+cd "$(dirname $0)/.."
+echo "$(dirname $0)"
+
+set -e
+
+source activate dfl
+
+echo "CONDA_DEFAULT_ENV=$CONDA_DEFAULT_ENV"
+if [ "$CONDA_DEFAULT_ENV" != "dfl" ]; then
+  echo "dfl env is not activated!"
+  exit 1
+fi
+
+conda info
+
+if [ ! -n "$DFL_MAIN" ]; then
+  conda env config vars set DFL_MAIN=".dfl/DeepFaceLab/main.py"
+  echo "set DFL_MAIN=.dfl/DeepFaceLab/main.py"
+else
+  echo "DFL_MAIN=$DFL_MAIN"
+fi
+
+if [ ! -n "$WORKSPACE" ]; then
+  conda env config vars set WORKSPACE="workspace"
+  echo "set WORKSPACE=workspace"
+else
+  echo "WORKSPACE=$WORKSPACE"
+fi
+
+source activate dfl
+
+echo "upgrading pip ..."
+python -m pip install --upgrade pip
+
+reqs_file="$(dirname $0)/requirements.txt"
+echo "Using $reqs_file for $(python -V)"
+
+pip --no-cache-dir install -r $reqs_file
+
+if [ ! -d workspace ]; then
+  mkdir -p workspace/data_src
+  mkdir -p workspace/data_dst
+  mkdir -p workspace/model
+fi
+
+echo "successfully install deepfacelab!"
+echo "good deepfacelabing ~"
+
